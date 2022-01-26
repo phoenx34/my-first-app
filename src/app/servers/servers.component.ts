fae8320b1b19cd4e89e09f66cddd3e49eea81cb6
phoenx34 <misaac34@gmail.com>
2022-01-26 14:00:41 -0500

    Two way Databinding Demo

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,7 +14,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus = 'No server was created.';
-  serverName = '';
+  serverName = 'Testserver';
 
   constructor() {
     setTimeout(() => {
@@ -26,7 +26,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: any) {
